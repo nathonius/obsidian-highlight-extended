ef786af2b138fbaded42a8a819892b4ef6d885bc
Nathan Smith <nathan@nathan-smith.org>
2021-09-23 20:45:51 -0400

    feat: support multiple files

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,9 +1,10 @@
 import { MarkdownPostProcessorContext, Plugin } from 'obsidian';
 
 type RenderLineEvent = CodeMirror.LineHandle & Pick<CodeMirror.EditorChange, 'from' | 'to'>;
+const ACTIVE_FILE = '__HIGHLIGHT_ACTIVE_FILE';
 
 export class TextColorsPlugin extends Plugin {
-  markedLines: Record<number, CodeMirror.TextMarker[]> = {};
+  markedLines: Record<string, Record<number, CodeMirror.TextMarker[]>> = {};
   async onload(): Promise<void> {
     this.registerMarkdownPostProcessor(this.processor.bind(this));
 
@@ -14,10 +15,14 @@ export class TextColorsPlugin extends Plugin {
   }
 
   private handleChange(instance: CodeMirror.Editor, changeObj: CodeMirror.EditorChangeLinkedList): void {
+    const file = this.app.workspace.getActiveFile();
+    const fileKey = file ? file.path : ACTIVE_FILE;
+    // console.log(instance.getDoc());
+    // console.log(instance.getEditor());
     // const pattern = /(\[.*?\])(\{.*?\})/g;
     // const pattern = /(==.*?==)(\[.*?\])/g;
     const pattern = /==[^=]*?==\[(.*?)\]/g;
-    console.log(changeObj);
+    // console.log(changeObj);
     const startRange = changeObj.from.line;
     const endRemoveRange = changeObj.to.line + 1;
     const endAddRange = startRange + changeObj.text.length;
@@ -44,20 +49,34 @@ export class TextColorsPlugin extends Plugin {
     //   unmarkLine(i);
     // }
 
+    if (fileKey === ACTIVE_FILE && this.markedLines[fileKey]) {
+      Object.keys(this.markedLines[fileKey]).forEach((k: any) => {
+        this.markedLines[fileKey][k].forEach((m) => {
+          m.clear();
+        });
+        delete this.markedLines[ACTIVE_FILE][k];
+      });
+      delete this.markedLines[ACTIVE_FILE];
+    }
+
     for (let i = startRange; i < endRange; i++) {
+      if (!this.markedLines[fileKey]) {
+        this.markedLines[fileKey] = {};
+      }
+
       // Unmark line
-      if (this.markedLines[i]) {
-        this.markedLines[i].forEach((marker) => {
+      if (this.markedLines[fileKey][i]) {
+        this.markedLines[fileKey][i].forEach((marker) => {
           marker.clear();
         });
-        delete this.markedLines[i];
+        delete this.markedLines[fileKey][i];
       }
 
       const marks: CodeMirror.TextMarker[] = [];
       const line = instance.getLine(i);
       let match: RegExpExecArray | null = null;
       while ((match = pattern.exec(line)) !== null) {
-        console.log(match);
+        // console.log(match);
         const start = match.index;
         const end = match.index + match[0].length;
         const color = match[1];
@@ -74,7 +93,7 @@ export class TextColorsPlugin extends Plugin {
         );
       }
       if (marks.length > 0) {
-        this.markedLines[i] = marks;
+        this.markedLines[fileKey][i] = marks;
       }
       // line.matchAll()
       // if (match) {
