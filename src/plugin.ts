15d0c19fb41776aeebbf61509f4e9020e144ee79
Nathan Smith <nathan@nathan-smith.org>
2021-09-23 20:51:59 -0400

    refactor: clean up code, document, etc

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,70 +1,43 @@
-import { MarkdownPostProcessorContext, Plugin } from 'obsidian';
-
-type RenderLineEvent = CodeMirror.LineHandle & Pick<CodeMirror.EditorChange, 'from' | 'to'>;
-const ACTIVE_FILE = '__HIGHLIGHT_ACTIVE_FILE';
+import { Plugin } from 'obsidian';
+import { ACTIVE_FILE, EDIT_MODE_PATTERN } from './constants';
 
 export class TextColorsPlugin extends Plugin {
   markedLines: Record<string, Record<number, CodeMirror.TextMarker[]>> = {};
   async onload(): Promise<void> {
-    this.registerMarkdownPostProcessor(this.processor.bind(this));
-
     this.registerCodeMirror((editor) => {
-      // editor.on('change', this.handleChange.bind(this));
       editor.on('change', this.handleChange.bind(this));
     });
   }
 
   private handleChange(instance: CodeMirror.Editor, changeObj: CodeMirror.EditorChangeLinkedList): void {
+    // Get key for current file
     const file = this.app.workspace.getActiveFile();
     const fileKey = file ? file.path : ACTIVE_FILE;
-    // console.log(instance.getDoc());
-    // console.log(instance.getEditor());
-    // const pattern = /(\[.*?\])(\{.*?\})/g;
-    // const pattern = /(==.*?==)(\[.*?\])/g;
-    const pattern = /==[^=]*?==\[(.*?)\]/g;
-    // console.log(changeObj);
+
+    // Compute ranges
     const startRange = changeObj.from.line;
     const endRemoveRange = changeObj.to.line + 1;
     const endAddRange = startRange + changeObj.text.length;
     const endRange = Math.max(endRemoveRange, endAddRange);
 
-    // Unmark lines
-    // for(let i = startRange; i < endRange; i++) {
-
-    // }
-    // const endRange = changeObj.to.line + changeObj.text.length - (changeObj.removed?.length || 1) + 1;
-    // const removedCount = (changeObj.removed?.length || 1) - 1;
-
-    // const unmarkLine = (lineNo: number) => {
-
-    // };
-
-    // // Unmark removed lines
-    // for (let i = startRange; i < removedCount + startRange; i++) {
-    //   unmarkLine(i);
-    // }
-
-    // // Unmark changed lines
-    // for (let i = startRange; i < endRange; i++) {
-    //   unmarkLine(i);
-    // }
-
+    // If file is null we need to clear all marks first.
     if (fileKey === ACTIVE_FILE && this.markedLines[fileKey]) {
-      Object.keys(this.markedLines[fileKey]).forEach((k: any) => {
-        this.markedLines[fileKey][k].forEach((m) => {
-          m.clear();
+      Object.keys(this.markedLines[fileKey]).forEach((key: string) => {
+        const lineNumber = parseInt(key);
+        this.markedLines[fileKey][lineNumber].forEach((mark) => {
+          mark.clear();
         });
-        delete this.markedLines[ACTIVE_FILE][k];
+        delete this.markedLines[ACTIVE_FILE][lineNumber];
       });
-      delete this.markedLines[ACTIVE_FILE];
     }
 
+    // Handle each changed line
     for (let i = startRange; i < endRange; i++) {
       if (!this.markedLines[fileKey]) {
         this.markedLines[fileKey] = {};
       }
 
-      // Unmark line
+      // Unmark affected lines
       if (this.markedLines[fileKey][i]) {
         this.markedLines[fileKey][i].forEach((marker) => {
           marker.clear();
@@ -72,18 +45,14 @@ export class TextColorsPlugin extends Plugin {
         delete this.markedLines[fileKey][i];
       }
 
+      // Mark affected lines
       const marks: CodeMirror.TextMarker[] = [];
       const line = instance.getLine(i);
       let match: RegExpExecArray | null = null;
-      while ((match = pattern.exec(line)) !== null) {
-        // console.log(match);
+      while ((match = EDIT_MODE_PATTERN.exec(line)) !== null) {
         const start = match.index;
         const end = match.index + match[0].length;
         const color = match[1];
-        // console.log(instance.getRange({ line: i, ch: start }, { line: i, ch: end }));
-        // const line = instance.getLineHandle(i);
-        // console.log(line);
-        // console.log(instance.getLineTokens(i));
         marks.push(
           instance.markText(
             { line: i, ch: start },
@@ -92,19 +61,11 @@ export class TextColorsPlugin extends Plugin {
           )
         );
       }
+
+      // Save marks if any were made
       if (marks.length > 0) {
         this.markedLines[fileKey][i] = marks;
       }
-      // line.matchAll()
-      // if (match) {
-      //   console.log(match);
-      //   // console.log(instance.getRange({ line: i, ch: match.index }, { line: i, ch: match.index + match[0].length }));
-      // }
     }
-  }
-
-  private async processor(element: HTMLElement, context: MarkdownPostProcessorContext): Promise<void> {
-    console.log(element);
-    console.log(context);
   }
 }
